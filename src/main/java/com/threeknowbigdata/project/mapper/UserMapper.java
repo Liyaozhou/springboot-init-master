d01d458feed767610aab9086a59503506d870dc7
liyaozhou01 <liyaozhou01@corp.netease.com>
2024-02-22 23:12:23 +0800

    更新代码

---
+++ src/main/java/com/threeknowbigdata/project/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.threeknowbigdata.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.threeknowbigdata.project.model.entity.User;
+
+/**
+ * @Entity com.threeknowbigdata.project.model.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
